dd3554c140b0a08a34bcf1272a8091d5360d4b4e
Vaceslav676 <vaceslavzubenko676@gmail.com>
2023-04-22 21:15:05 +0300

    подкоректировал слушатели

--- app/src/main/java/android/example/jastjava/Activity2.java
+++ app/src/main/java/android/example/jastjava/Activity2.java
@@ -3,6 +3,7 @@ package android.example.jastjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -11,4 +12,10 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
     }
+
+    public void onClick2(View view) {
+
+
+    }
+
 }
--- app/src/main/java/android/example/jastjava/Activity3.java
+++ app/src/main/java/android/example/jastjava/Activity3.java
@@ -3,6 +3,7 @@ package android.example.jastjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity3 extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class Activity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+    }
+
+    public void onClick3(View view) {
+
+
     }
 }
