c060e21e3fc7d1ea3925200b1eb4a9a0189a9af4
Vaceslav676 <vaceslavzubenko676@gmail.com>
2023-04-22 21:11:08 +0300

    Создал три чистых активити

--- app/src/main/java/android/example/jastjava/MainActivity.java
+++ app/src/main/java/android/example/jastjava/MainActivity.java
@@ -16,109 +16,18 @@ import android.widget.Toast;
 
 import java.text.NumberFormat;
 
-// Это приложение отображает форму заказа для заказа кофе.
+// Ссылка на ютуб урок
+// https://www.youtube.com/watch?v=lyGGar6U-HY
 public class MainActivity extends AppCompatActivity {
-    int numberOfCoffees = 2;
-    int cennik = 50;
-    int cennikChokolad;
-    int cennikCream;
-    int totalPrice;
-    private Toast plusToast;
-    private Toast minusToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //Создаем переменную и привязываем ее к текст вью
-    // Внимание!: тут название переменной и название текствью совпадают
-    //TextView quantitytextview = (TextView) findViewById(R.id.quantitytextview);
 
-    // Создадим переменную чтобы задавать начальное количество чашек кофе из
-    // текстового поля quantitytextview.
-    // но нужно преобразовать из текста в число из текст quantitytextview
-   //  long defNumbCoff = Long.parseLong(quantitytextview.getText().toString());
+    public void onClick1(View view) {
 
-    //////////////////////////////////////////////////////////////////////
-    // Метод создания отдельной переменной для чек бокса
-    // хотя в чекбоксе есть встроенная переменная
-    public boolean onCheckBoxClicked(View view) {
-        CheckBox checkBox11 = findViewById(R.id.checkbox1);
-        boolean isCheckBox11 = checkBox11.isChecked();
-       // Если чекбокс истина то ценник за шоколад 20
-        if (isCheckBox11) {cennikChokolad = 20;
-        }else {cennikChokolad = 0;}// иначе 0
-        return isCheckBox11;
-    }
-
-    public boolean onCheckBoxClicked2(View view) {
-        CheckBox checkBox22 = findViewById(R.id.checkbox2);
-        boolean isCheckBox22 = checkBox22.isChecked();
-        // Если чекбокс истина то ценник за сливки 15
-        if (isCheckBox22) {
-            cennikCream = 15; // иначе 0
-        }else {cennikCream = 0;}
-
-        return isCheckBox22;
-    }
- /////////////////////////////////////////////////////////////////////////
-
-    // Этот метод вызывается при нажатии кнопки " + "..
-    // This metod is called when the " + " button is clicked.
-    public void submitOrder(View view) {
-        if (numberOfCoffees >= 99) {
-            plusToast = Toast.makeText(getBaseContext(),
-                    "нельзя заказать более 99 кофе", Toast.LENGTH_SHORT);
-            plusToast.show();
-        }else {
-          display(++numberOfCoffees);}
 
     }
-    // Этот метод вызывается при нажатии кнопки " - ".
-    public void submitOrder2(View view) {
-        if(numberOfCoffees <= 1) {
-            minusToast = Toast.makeText(getBaseContext(),
-                    "нельзя заказать меньше 1 чашки", Toast.LENGTH_SHORT);
-            minusToast.show();
-        } else {
-        display(--numberOfCoffees); }
-    }
-
-    // Этот метод Заполняет окно "Количество" на экране
-    private void display(int AAA) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantitytextview);
-        quantityTextView.setText(AAA + " чаш. кофе");
-    }
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-    // Этот метод будет обновлять конечное поле с учетом чек бокса
-    // При нажатии кнопки Order (Заказать)
-    // сумма по чеку = (ценник за чашку + шоколад + сливки) * количество чашек
-    // ценник же = ценник за кофе + ценник за шокол + ценник за сливки
-    public void orderTotal(View view) {
-
-        totalPrice = (cennik + cennikChokolad + cennikCream) * numberOfCoffees;
-        displayTotalPrice(createOrderSummery(totalPrice, numberOfCoffees, view));
-    }
-
-    // Этот метод заполняет окно тоталПрайс на экране.
-    // This metod displays the given PRICE value on the screen.
-    private void displayTotalPrice(String message) {
-        TextView totalPriceTextView = (TextView) findViewById(R.id.totalPrice_text_view);
-        totalPriceTextView.setText(message);
-  }
-
-    //Этот метод формирует сообщение столбцом в несколько строк
-    private String createOrderSummery(int totalPrice, int numberOfCoffees, View view) {
-        String message = "Имя: " + ((EditText)findViewById(R.id.name_text_view)).getText();
-        message += "\nВзбитые сливки? " + onCheckBoxClicked(view);
-        message += "\nШоколад? " + onCheckBoxClicked2(view);
-        message += "\nКолличество: " + numberOfCoffees;
-        message += "\nЦена: " + totalPrice + " рублей";
-        message += "\nСпасибо!";
-        return message;
-    }
-
 }
