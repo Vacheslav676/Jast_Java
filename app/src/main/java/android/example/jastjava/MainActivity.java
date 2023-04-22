c75299575775a23a3255b0245b87a5135f89beab
Vaceslav676 <vaceslavzubenko676@gmail.com>
2023-04-22 22:30:20 +0300

    попытка

--- app/src/main/java/android/example/jastjava/MainActivity.java
+++ app/src/main/java/android/example/jastjava/MainActivity.java
@@ -9,6 +9,7 @@ package android.example.jastjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -18,8 +19,6 @@ import android.widget.Toast;
 
 import java.text.NumberFormat;
 
-// Ссылка на ютуб урок
-// https://www.youtube.com/watch?v=lyGGar6U-HY
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -28,30 +27,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick1(View view) {
-        // Создаю интент и говорю что хочу перейти из этого (this) активити,
-        // в активити2
-        Intent i = new Intent(MainActivity.this, Activity2.class);
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:47.6,-122.3?z=11"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
 
-        // Отправим данные
-        // Отправляем данные с ключевыми словами
-        i.putExtra("key_1", "Привет");
-        i.putExtra("key_2", 100);
-        i.putExtra("key_3", true);
-
-
-
-        //специальная функция для старта интента. После нее текущая активити
-        // не закрывается, а сворачивается
-        startActivity(i);
 
     }
 
-
-        // коментарии на Активити3 джава
-        @Override
-        public void onBackPressed(){
-            finish();
-        }
 
     }
